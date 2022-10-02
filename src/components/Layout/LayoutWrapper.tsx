d8986ddb13fa97a3f35d8da7e95e59de1828b74a
Yaşar Özyurt <blueromans@hotmail.com>
2022-10-03 00:32:33 +0300

    feat:Version updated.

--- src/components/Layout/LayoutWrapper.tsx
+++ src/components/Layout/LayoutWrapper.tsx
@@ -1,46 +1,14 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import color from 'color';
 
 import LayoutScroll from './LayoutScroll';
 import Layout from './Layout';
 import { withTheme } from '../../core/theming';
 
 import type { Props } from './types';
-import { SnackBarWrapper } from '../SnackBar';
 
 const LayoutWrapper = (props: Props) => {
-  const handleSnackBar = (ref: any) => SnackBarWrapper.setRef(ref);
-  const {
-    scroll,
-    theme,
-    statusBarStyle = 'light-content',
-    statusBarColor,
-    snackBarStyle = { titleStyle: {}, subTitleStyle: {} },
-    ...rest
-  } = props;
-  const statusBarBackground = statusBarColor
-    ? statusBarColor
-    : color(theme?.colors?.primary).darken(0.3).hex();
-  return (
-    <React.Fragment>
-      <StatusBar
-        barStyle={statusBarStyle}
-        backgroundColor={statusBarBackground}
-      />
-      {scroll ? (
-        <LayoutScroll {...props} {...rest} />
-      ) : (
-        <Layout {...props} {...rest} />
-      )}
-      <SnackBarWrapper
-        theme={theme}
-        titleStyle={snackBarStyle?.titleStyle}
-        subTitleStyle={snackBarStyle?.subTitleStyle}
-        ref={handleSnackBar}
-      />
-    </React.Fragment>
-  );
+  const { scroll } = props;
+  return scroll ? <LayoutScroll {...props} /> : <Layout {...props} />;
 };
 
 export default withTheme(LayoutWrapper);
